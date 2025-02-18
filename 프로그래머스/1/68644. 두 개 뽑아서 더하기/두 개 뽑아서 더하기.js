e40620a4f0e9c1cefc719589f277b0e523277fb1
sylvia <91243651+yookeunbyul@users.noreply.github.com>
2025-02-18 16:08:06 +0900

    [level 1] Title: 두 개 뽑아서 더하기, Time: 0.58 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
+++ 프로그래머스/1/68644. 두 개 뽑아서 더하기/두 개 뽑아서 더하기.js
@@ -1,12 +1,10 @@
 function solution(numbers) {
-    //두개의 수를 뽑아 더해서 만들 수 있는 모든 수를 배열에 오름차순으로 return
-    const answer = [];
-    
+    const answer = new Set();
     for(let i=0; i<numbers.length; i++){
         for(let j=i+1; j<numbers.length; j++){
-            answer.push(numbers[i] + numbers[j]);
+            answer.add(numbers[i] + numbers[j]);
         }
     }
     
-    return [...new Set(answer)].sort((a,b) => a-b);
+    return [...answer].sort((a,b) => a-b);
 }
